e225c93e4271d92cf591b5a561a70304540626cc
hashank <hashank@calcey.com>
2019-09-11 11:46:41 +0530

    Initial Commit

---
+++ src/ALMaster.Api/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ALMaster.Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
